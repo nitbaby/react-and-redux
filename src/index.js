448eec182aa816681d8eae10ba06e2164e215192
nitbaby <nitbaby@gmail.com>
2021-06-13 20:57:14 +0530

    added comment and imageUrl props
    Signed-off-by: nitbaby <nitbaby@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,9 +10,21 @@ if (module.hot) {
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" />
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" />
+      <CommentDetail 
+        author="Sam" 
+        timeAgo="Today at 4:45PM" 
+        imgUrl={faker.image.image()}
+        comment="Nice tutorial"/>
+      <CommentDetail 
+        author="Alex" 
+        timeAgo="Today at 2:00AM" 
+        imgUrl={faker.image.image()}
+        comment="What a nice day!" />
+      <CommentDetail 
+        author="Jane" 
+        timeAgo="Yesterday at 5:00PM" 
+        imgUrl={faker.image.image()}
+        comment="Lets fly away." />
     </div>
   );
 };
