3959a21ef5665573a4dca7508057bbc5e968338e
nitbaby <nitbaby@gmail.com>
2021-06-16 00:18:55 +0530

    Reusable components: pass a component to another

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 if (module.hot) {
   module.hot.accept();
@@ -10,21 +11,31 @@ if (module.hot) {
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 4:45PM" 
-        imgUrl={faker.image.image()}
-        comment="Nice tutorial"/>
-      <CommentDetail 
-        author="Alex" 
-        timeAgo="Today at 2:00AM" 
-        imgUrl={faker.image.image()}
-        comment="What a nice day!" />
-      <CommentDetail 
-        author="Jane" 
-        timeAgo="Yesterday at 5:00PM" 
-        imgUrl={faker.image.image()}
-        comment="Lets fly away." />
+      <ApprovalCard>
+        Are you sure?
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 4:45PM" 
+          imgUrl={faker.image.image()}
+          comment="Nice tutorial"/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Today at 2:00AM" 
+          imgUrl={faker.image.image()}
+          comment="What a nice day!" />
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Yesterday at 5:00PM" 
+          imgUrl={faker.image.image()}
+          comment="Lets fly away." />
+      </ApprovalCard>      
     </div>
   );
 };
